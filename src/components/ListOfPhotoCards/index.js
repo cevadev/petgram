26287584c526b371681eac85f605988b0c5be47f
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2020-12-07 12:45:09 -0300

    [add] Guardando likes en el localstorage

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -8,7 +8,8 @@ export const ListOfPhotoCards = () => {
   return (
     <List>
       {[1, 2, 3, 4, 5, 6, 7, 8].map((id) => (
-        <PhotoCard key={id} />
+        /**pasamos el id del elemento para crear un key unica y guardarlo en el local storage */
+        <PhotoCard key={id} id={id} />
       ))}
     </List>
   );
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -3,7 +3,9 @@ import React, { Fragment, useEffect, useRef, useState } from "react";
 import { ImgWrapper, Img, Button, Article } from "./styles.js";
 
 //importamos iconos de material design de react-icons
-import { MdFavoriteBorder } from "react-icons/md";
+//MdFavoriteBorder -> icono favorito bordeado
+//MdFavorite > icono favorito completo o relleno
+import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
 
 const DEFAULT_IMAGE =
   "https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png";
@@ -16,6 +18,20 @@ export const PhotoCard = ({ id, likes = 0, srcImage = DEFAULT_IMAGE }) => {
 
   //añadimos un state local al componente (article). Cuando show es true hacemos que se visualice el componente
   const [show, setShow] = useState(false);
+
+  //creamos nuestra key llamada (like + id del elemento en el array) para ser guardada en el local storage
+  const key = `like-${id}`;
+
+  //añadimos un state local para manejar los likes.
+  const [liked, setLiked] = useState(() => {
+    //Revisamos el local storage para recuperar el valor de la key unica llamada key
+    try {
+      const like = window.localStorage.getItem(key);
+      return JSON.parse(like);
+    } catch (error) {
+      console.error(error);
+    }
+  });
 
   /**
    * useEffect que se va ejecutar solamente cuando cambie el valor de domElement
@@ -51,6 +67,22 @@ export const PhotoCard = ({ id, likes = 0, srcImage = DEFAULT_IMAGE }) => {
     [domElement]
   );
 
+  //si le damos click en el icono favorito, mostramos el icono favorito relleno
+  //la primera letra de la constate es mayuscula porque así react entiende que estamos renderizando un componente
+  const Icon = liked ? MdFavorite : MdFavoriteBorder;
+
+  //funcion para guardar likes en el local storage
+  const setLocalStorage = (value) => {
+    try {
+      //local storage maneja es un map de clave / valor
+      window.localStorage.setItem(key, value);
+      //actualizamso el local state
+      setLiked(value);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   //la props ref es una prop especial de react que nos permite capturar el elemento del dom por lo tanto en la variable ref
   //guardamos dicha referencia del elemento del dom
 
@@ -65,8 +97,9 @@ export const PhotoCard = ({ id, likes = 0, srcImage = DEFAULT_IMAGE }) => {
               <Img src={srcImage} />
             </ImgWrapper>
           </a>
-          <Button type="button">
-            <MdFavoriteBorder size="25" />
+          {/**establecemos el valor de liked con el valor contrario que posea */}
+          <Button type="button" onClick={() => setLocalStorage(!liked)}>
+            <Icon size="25" />
             {likes} likes
           </Button>
         </Fragment>
