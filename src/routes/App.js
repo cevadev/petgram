188bafd79f2026a50dd3d044cfbe4ea98c9fa1a0
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2020-12-09 17:40:48 -0300

    [Add] Rutas protegidas

--- src/routes/App.js
+++ src/routes/App.js
@@ -3,11 +3,22 @@ import { Router } from "@reach/router";
 
 import { Home } from "../pages/Home.js";
 import { Detail } from "../pages/Detail.js";
+import { Favs } from "../pages/Favs.js";
+import { User } from "../pages/User.js";
+import { NotRegisteredUser } from "../pages/NotRegisteredUser.js";
 import Logo from "../components/Logo/index.js";
 import { NavBar } from "../components/NavBar/index.js";
 
 //import estilos globales
 import { GlobalStyle } from "../styles/GlobalStyles.js";
+
+/**
+ * Si el usuario está registrado por ver las paginas de favs y user, si no esta registrado verá solo el home
+ * UserLogged va recibir el children, el children sera lo que tiene que renderizar como una funcion
+ */
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false });
+};
 
 function App() {
   return (
@@ -19,6 +30,23 @@ function App() {
         <Home path="/pet/:categoryId" />
         <Detail path="/detail/:detailId" />
       </Router>
+      <UserLogged>
+        {/**rederizado condicional, a UserLogged le pasamos el children*/}
+        {({ isAuth }) =>
+          /**si esta autenticado, renderizamos un router con acceso a favs y user de lo contrario NotRegisteredUser */
+          isAuth ? (
+            <Router>
+              <Favs path="/favs" />
+              <User path="/user" />
+            </Router>
+          ) : (
+            <Router>
+              <NotRegisteredUser path="/favs" />
+              <NotRegisteredUser path="/user" />
+            </Router>
+          )
+        }
+      </UserLogged>
       <NavBar />
     </div>
   );
