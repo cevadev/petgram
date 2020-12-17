e7f33b50bd43d45d96b4eca892d5c21c56569a1a
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2020-12-17 12:22:03 -0300

    [add] working with Service Worker and Offline mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
-
+const WebpackPwaManifestPlugin = require("webpack-pwa-manifest");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   //entry-point: archivo principal o punto de entrada que leera webpack
@@ -44,6 +45,53 @@ module.exports = {
       template: "./public/index.html",
       //archivo final producido
       filename: "./index.html",
+    }),
+    new WebpackPwaManifestPlugin({
+      //definimos el objeto de configuracion
+      name: "Petgram - Tu app de fotos de mascotas",
+      short_name: "Petgram",
+      description: "Con Petgram puedes encontrar fotos de animales domésticos",
+      background_color: "#fff",
+      theme_color: "#b1a",
+      icons: [
+        {
+          src: path.resolve(__dirname, "src/assets/icon.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+        },
+      ],
+    }),
+    //generamos un service worker
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: "service-worker.js",
+      clientsClaim: true,
+      skipWaiting: true,
+      maximumFileSizeToCacheInBytes: 5000000,
+      //indicamos lo que el SW necesita hacer cache
+      runtimeCaching: [
+        {
+          //desde esta url cargamos las imagenes
+          urlPattern: new RegExp(
+            "https://(res.cloudinary.com|images.unplash.com)"
+          ),
+          //CacheFirst-> primero verificarar si el recurso esta en el cache, si no es asi lo recuperar
+          handler: "CacheFirst",
+          options: {
+            cacheName: "images",
+          },
+        },
+        //chache para la api
+        {
+          //url del servidor que contiene nuestra api o backend
+          urlPattern: new RegExp(
+            "https://petgram-server-api-barcvilla-l8ya7qy99.vercel.app"
+          ),
+          //CacheFirst-> primero verificarar si el recurso esta en el cache, si no es asi lo recuperar
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "api",
+          },
+        },
+      ],
     }),
   ],
   //creamos un servidor de trabajo local y ver los cambios en la app
